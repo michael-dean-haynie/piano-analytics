8edb043a63d9eaef7d118eae4eadc64070785ebc
Michael Haynie <michael.dean.haynie@gmail.com>
2023-07-02 10:38:14 -0700

    add login page

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FeathersService} from "../feathers.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  constructor(
+    private feathersService: FeathersService,
+    private router: Router
+  ) {}
 
+  username: string = ''
+  password: string = ''
+
+  async login(): Promise<void> {
+    try {
+      await this.feathersService.client.authenticate({
+        strategy: 'local',
+        email: this.username,
+        password: this.password
+      });
+      await this.router.navigate(['/'])
+    } catch (error) {
+      console.log(error)
+    }
+  }
 }
